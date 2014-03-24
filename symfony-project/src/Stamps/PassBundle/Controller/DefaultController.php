58482128c8f45d570b26b1605099ac981c267c37
yene <yene@cocoadojo.com>
2014-03-24 21:52:33 +0100

    successful added PKPass

--- symfony-project/src/Stamps/PassBundle/Controller/DefaultController.php
+++ symfony-project/src/Stamps/PassBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace Stamps\PassBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use PKPass;
 
 class DefaultController extends Controller {
 	public function indexAction($name) {
@@ -11,7 +12,7 @@ class DefaultController extends Controller {
 		$userAgent = $request->headers->get('User-Agent') . "Passbook";
 		if (strpos($userAgent,'Passbook') !== false) {
 			$serialNumber = $this->generateSerialNumber();
-			//$this->generatePass($serialNumber);
+			$passData = $this->generatePass($serialNumber);
 
 			return $this->render('StampsPassBundle:Default:index.html.twig', array('name' => $userAgent));
 		} else {
@@ -21,21 +22,26 @@ class DefaultController extends Controller {
 
 
 	function generatePass($serialNumber) {
-
-		//$loader = require __DIR__.'/../../../../vendor/autoload.php';
-		//$loader->add('PKPass\PKPass', __DIR__.'pkpass/pkpass/src');
-
 		$pass = new PKPass\PKPass();
-		$pass->setCertificate('../Certificate.p12');  // 2. Set the path to your Pass Certificate (.p12 file)
+		$pass->setCertificate(__DIR__ . '/../Resources/config/Certificate.p12');  // 2. Set the path to your Pass Certificate (.p12 file)
 		$pass->setCertificatePassword('');     // 2. Set password for certificate
-		$pass->setWWDRcertPath(@StampsPassBundle/Resources/config/AppleWWDRCA.pem); // 3. Set the path to your WWDR Intermediate certificate (.pem file)
-		$pass->setJSON(json_encode($passData));
+		$pass->setWWDRcertPath(__DIR__ . '/../Resources/config/AppleWWDRCA.pem'); // 3. Set the path to your WWDR Intermediate certificate (.pem file)
 
+
+		$passJson = file_get_contents(__DIR__ . '/../Resources/config/souperbe.pass/pass.json');
+		$passData = json_decode($passJson);
+
+		// modify the pass template
+		$passData->{'serialNumber'} = $serialNumber;
+		$passData->{'barcode'}->{'message'} = $serialNumber;
+		$passData->{'barcode'}->{'altText'} = $serialNumber;
+
+		$pass->setJSON(json_encode($passData));
 
 		// Add files to the PKPass package
 
 		// add all images from the template folder
-		$files = glob('../souperbe.pass/*.{jpg,png,gif}', GLOB_BRACE);
+		$files = glob(__DIR__ . '/../Resources/config/souperbe.pass/*.{jpg,png,gif}', GLOB_BRACE);
 		foreach($files as $file) {
 			$pass->addFile($file);
 		}
@@ -45,14 +51,12 @@ class DefaultController extends Controller {
 		//$pass->addFile('../souperbe.pass/icon@2x.png');
 		//$pass->addFile('../souperbe.pass/logo.png');
 
-		if(!$pass->create(true)) { // Create and output the PKPass
-			echo 'Error: '.$pass->getError();
+		$passOutput = $pass->create(true);
+		if(!$passOutput) { // Create and output the PKPass
+			throw new \Exception('Error: '.$pass->getError());
 		}
-
+		return $passOutput;
 	}
-
-
-
 
 	function generateSerialNumber(){
 	  $serialNumber = substr(md5(uniqid(mt_rand(), true)), 0, 9); // No two passes with the same pass type identifier may have the same serial number.
